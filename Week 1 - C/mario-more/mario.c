5c77f5bdab5e521ff0740da013dcb067d53cfc99
Darren Massey <phailhaus@gmail.com>
2023-07-16 15:31:51 +0000

    Removed unused include from Mario

--- Week 1 - C/mario-more/mario.c
+++ Week 1 - C/mario-more/mario.c
@@ -1,8 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
 
-#include <string.h>
-
 int get_int_bounded(string message, bool lowerboundbool, int lowerbound, bool upperboundbool, int upperbound);
 void printchars(char symbol, int length);
 
