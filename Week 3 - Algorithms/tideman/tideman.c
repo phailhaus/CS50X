c2eaecc0c149654c53bd82a155429f3e4738eaaa
Darren Massey <phailhaus@gmail.com>
2023-07-22 01:22:46 +0000

    Finished week 3 with Tideman and the Painful Chains

--- Week 3 - Algorithms/tideman/tideman.c
+++ Week 3 - Algorithms/tideman/tideman.c
@@ -134,7 +134,6 @@ void add_pairs(void)
         for (int j = i + 1; j < candidate_count; j++)
         {
             int matchup_score = preferences[i][j] - preferences[j][i];
-            printf("%s vs %s - %i\n", candidates[i], candidates[j], matchup_score);
             if (matchup_score > 0)
             {
                 pairs[pair_count].winner = i;
@@ -162,7 +161,7 @@ void sort_pairs(void)
     bool sorted = false;
     int loops = 0;
 
-    while(true)
+    while (true)
     {
         sorted = true;
         for (int i = 0; i < pair_count - loops - 1; i++)
@@ -170,23 +169,19 @@ void sort_pairs(void)
             pair pair_1 = pairs[i];
             int matchup_score_1 = preferences[pair_1.winner][pair_1.loser] - preferences[pair_1.loser][pair_1.winner];
 
-            pair pair_2 = pairs[i+1];
+            pair pair_2 = pairs[i + 1];
             int matchup_score_2 = preferences[pair_2.winner][pair_2.loser] - preferences[pair_2.loser][pair_2.winner];
 
             if (matchup_score_1 < matchup_score_2)
             {
                 sorted = false;
                 pairs[i] = pair_2;
-                pairs[i+1] = pair_1;
+                pairs[i + 1] = pair_1;
             }
         }
 
         if (sorted)
         {
-            for (int i = 0; i < pair_count; i++)
-            {
-                printf("%s beats %s\n", candidates[pairs[i].winner], candidates[pairs[i].loser]);
-            }
             return;
         }
 
@@ -196,44 +191,59 @@ void sort_pairs(void)
     return;
 }
 
-// Recursively check earlier pairs to see if current loser previously beat the current winner
-bool chain_check(int pairs_position, int target_candidate)
-{
-    printf("Checking to see if pair %i has %s lose\n", pairs_position, candidates[target_candidate]);
-    if (pairs[pairs_position].loser == target_candidate) // If the winner we were checking lost to somebody down the chain from its loser, send up true
-    {
-        return true;
-    }
-
-    for (int i = pairs_position - 1; i >= 0; i--) // Check every pair stronger than this
-    {
-        if (pairs[i].winner == pairs[pairs_position].loser && locked[pairs[i].winner][pairs[i].loser] == true) // If that pair has the loser of this one win and has itself been locked, check it
-        {
-            if (chain_check(i, target_candidate))
-            {
-                return true;
-            }
-        }
-    }
-
-    return false;
-}
-
-
 // Lock pairs into the candidate graph in order, without creating cycles
 void lock_pairs(void)
 {
-    for (int i = 0; i < pair_count; i++)
+    for (int i = 0; i < pair_count; i++) // Work through all the pairs from strongest to weakest
     {
-        printf("Checking pair %i\n", i);
-        if (!chain_check(i, pairs[i].winner))
+        bool been_beaten[candidate_count]; // Initialize an array that will be used when tracing chains
+        for (int j = 0; j < candidate_count; j++)
         {
-            printf("Pair %i locked\n", i);
+            been_beaten[j] = false;
+        }
+
+        if (i > 0)
+        {
+            bool has_won = false;
+            bool has_lost = false;
+            // Optimization that skips the chain check
+            // if the loser has never won or the winner has never lost in stronger matchups
+            for (int j = i - 1; j >= 0; j--)
+            {
+                if (locked[pairs[j].winner][pairs[j].loser] == true && pairs[j].loser == pairs[i].winner)
+                {
+                    has_lost = true;
+                }
+                if (locked[pairs[j].winner][pairs[j].loser] == true && pairs[j].winner == pairs[i].loser)
+                {
+                    has_won = true;
+                }
+            }
+
+            if (has_won == true && has_lost == true)
+            {
+                // Chains can't be longer than the number of candidates
+                // so do that number of passes through the pairs
+                for (int j = 0; j < candidate_count; j++)
+                {
+                    for (int k = i - 1; k >= 0; k--) // Check every pair prior to the one that is in the process of being locked
+                    {
+                        // Check if the pair is locked and the winner is either the loser we are looking for
+                        // or someone who has been beaten by them in a prior check
+                        if (locked[pairs[j].winner][pairs[j].loser] == true && (pairs[k].winner == pairs[i].loser || been_beaten[pairs[k].winner] == true))
+                        {
+                            been_beaten[pairs[k].loser] = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (been_beaten[pairs[i].winner] == false)
+        {
             locked[pairs[i].winner][pairs[i].loser] = true;
         }
     }
-
-    return;
 }
 
 // Print the winner of the election
